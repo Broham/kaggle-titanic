36043ef7efdc1599c5a86d28a923f04ee61cfdec
Abe Miessler <abe@verbalink.com>
2014-10-02 09:44:30 -0700

    changes to first forest

--- kaggle/myfirstforest.py
+++ kaggle/myfirstforest.py
@@ -8,6 +8,7 @@ please see packages.python.org/milk/randomforests.html for more
 import pandas as pd
 import numpy as np
 import csv as csv
+import re
 from sklearn.ensemble import RandomForestClassifier
 
 # Data cleanup
@@ -23,22 +24,36 @@ train_df['Gender'] = train_df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
 # Embarked from 'C', 'Q', 'S'
 # Note this is not ideal: in translating categories to numbers, Port "2" is not 2 times greater than Port "1", etc.
 
+
 # All missing Embarked -> just make them embark from most common place
 if len(train_df.Embarked[ train_df.Embarked.isnull() ]) > 0:
     train_df.Embarked[ train_df.Embarked.isnull() ] = train_df.Embarked.dropna().mode().values
 
-Ports = list(enumerate(np.unique(train_df['Embarked'])))    # determine all values of Embarked,
-Ports_dict = { name : i for i, name in Ports }              # set up a dictionary in the form  Ports : index
-train_df.Embarked = train_df.Embarked.map( lambda x: Ports_dict[x]).astype(int)     # Convert all Embark strings to int
+# Ports = list(enumerate(np.unique(train_df['Embarked'])))    # determine all values of Embarked,
+# Ports_dict = { name : i for i, name in Ports }              # set up a dictionary in the form  Ports : index
+# train_df.Embarked = train_df.Embarked.map( lambda x: Ports_dict[x]).astype(int)     # Convert all Embark strings to int
+train_df = pd.concat([train_df, pd.get_dummies(train_df['Embarked']).rename(columns=lambda x: 'Embarked_' + str(x))], axis=1)
 
 # All the ages with no data -> make the median of all Ages
 median_age = train_df['Age'].dropna().median()
 if len(train_df.Age[ train_df.Age.isnull() ]) > 0:
     train_df.loc[ (train_df.Age.isnull()), 'Age'] = median_age
 
-# Remove the Name column, Cabin, Ticket, and Sex (since I copied and filled it to Gender)
-train_df = train_df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId'], axis=1) 
+#Custom Features
+train_df["FamilySize"] = train_df["SibSp"] + train_df["Parch"]
+train_df["AgeClass"] = train_df["Age"]*train_df["Pclass"]
 
+#Title feature:
+train_df["Title"] = train_df['Name'].map(lambda x: re.search('.*, (.*?\.).*',x).group(1))
+train_df = pd.concat([train_df, pd.get_dummies(train_df['Title']).rename(columns=lambda x: 'Title_' + str(x))], axis=1)
+#Grouping and examining title:
+# gb = df.groupby(["Survived","title"])
+# gb.title.count()[0]
+# gb.title.count()[1]
+
+
+# Remove the Name column, Cabin, Ticket, and Sex (since I copied and filled it to Gender)
+train_df = train_df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId', 'Embarked', 'Title'], axis=1) 
 
 # TEST DATA
 test_df = pd.read_csv('test.csv', header=0)        # Load the test file into a dataframe
@@ -48,12 +63,15 @@ test_df = pd.read_csv('test.csv', header=0)        # Load the test file into a d
 # female = 0, Male = 1
 test_df['Gender'] = test_df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
 
+
 # Embarked from 'C', 'Q', 'S'
 # All missing Embarked -> just make them embark from most common place
-if len(test_df.Embarked[ test_df.Embarked.isnull() ]) > 0:
-    test_df.Embarked[ test_df.Embarked.isnull() ] = test_df.Embarked.dropna().mode().values
-# Again convert all Embarked strings to int
-test_df.Embarked = test_df.Embarked.map( lambda x: Ports_dict[x]).astype(int)
+# if len(test_df.Embarked[ test_df.Embarked.isnull() ]) > 0:
+#     test_df.Embarked[ test_df.Embarked.isnull() ] = test_df.Embarked.dropna().mode().values
+# # Again convert all Embarked strings to int
+# test_df.Embarked = test_df.Embarked.map( lambda x: Ports_dict[x]).astype(int)
+test_df = pd.concat([test_df, pd.get_dummies(test_df['Embarked']).rename(columns=lambda x: 'Embarked_' + str(x))], axis=1)
+
 
 
 # All the ages with no data -> make the median of all Ages
@@ -69,11 +87,30 @@ if len(test_df.Fare[ test_df.Fare.isnull() ]) > 0:
     for f in range(0,3):                                              # loop 0 to 2
         test_df.loc[ (test_df.Fare.isnull()) & (test_df.Pclass == f+1 ), 'Fare'] = median_fare[f]
 
+#Custom features
+test_df["FamilySize"] = test_df["SibSp"] + test_df["Parch"]
+test_df["AgeClass"] = test_df["Age"]*test_df["Pclass"]
+
+#Title feature:
+test_df["Title"] = test_df['Name'].map(lambda x: re.search('.*, (.*?\.).*',x).group(1))
+test_df = pd.concat([test_df, pd.get_dummies(test_df['Title']).rename(columns=lambda x: 'Title_' + str(x))], axis=1)
+
+
 # Collect the test data's PassengerIds before dropping it
 ids = test_df['PassengerId'].values
 # Remove the Name column, Cabin, Ticket, and Sex (since I copied and filled it to Gender)
-test_df = test_df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId'], axis=1) 
+test_df = test_df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId', 'Embarked', 'Title'], axis=1) 
 
+common_cols = [col for col in set(train_df.columns).intersection(test_df.columns)]
+test_df = test_df[common_cols]
+common_cols.insert(0,'Survived')
+train_df = train_df[common_cols]
+
+# print test_df	
+# print train_df
+
+# print list(test_df)
+# print list(train_df)
 
 # The data is now ready to go. So lets fit to the train, then predict to the test!
 # Convert back to a numpy array
